d1e1c39e794c615969366200fb1d69ddd0e0492a
TomGrooves <kasperfryd@gmail.com>
2020-09-18 10:39:20 +0200

    Updates

---
+++ precache-manifest.843cf95e7dde7f5abc7e8de9a5755791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3848d5462c3a6adce0ed4482690d8b1",
+    "url": "/stringsonline/index.html"
+  },
+  {
+    "revision": "6d426005e35db36e1f35",
+    "url": "/stringsonline/static/css/main.c64a55c4.chunk.css"
+  },
+  {
+    "revision": "b0a3c2b5f62feb3240ed",
+    "url": "/stringsonline/static/js/2.ac5ce766.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/stringsonline/static/js/2.ac5ce766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d426005e35db36e1f35",
+    "url": "/stringsonline/static/js/main.fcb355c0.chunk.js"
+  },
+  {
+    "revision": "23eaf9f7701217542ab6",
+    "url": "/stringsonline/static/js/runtime-main.2a3026bc.js"
+  },
+  {
+    "revision": "e6058fbe79ed8e914e2d2e61da02d192",
+    "url": "/stringsonline/static/media/hero-banner.e6058fbe.png"
+  }
+]);
