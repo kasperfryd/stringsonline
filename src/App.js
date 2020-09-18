c39e381f42677476dda4153bf394d03e5449d92e
TomGrooves <kasperfryd@gmail.com>
2020-09-18 10:33:14 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -6,35 +6,6 @@ import Header from './components/header/header'
 import Footer from './components/footer/footer';
 import Routes from './routes/routes';
 
-// TODO:
-
-
-// Comments
-// Fix double entry i kurv
-
-
-// Cleanup
-// Mobil styling
-// styling
-// Ratings
-// Content on termspage
-// Sortering
-// OrderHistory page
-// Offerprice
-// Order completed page
-// Checkout page
-// Cart Page (DONE)
-// Brands page (DONE)
-// Footer component (DONE)
-// Seachresult page (DONE)
-// Frontpage page (DONE)
-// Header component (DONE)
-// Navigation (aside) Component (DONE)
-// Breadcrumb Component (DONE)
-// Product page (DONE)
-// Product details page (DONE)
-// Login page (DONE)
-
 function App() {
   return (
     <Router>
